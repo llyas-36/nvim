c5efcc83b7d3bca6594db0da1597ed48cfa68848
llyas36 <eliyasinbox1@gmail.com>
2025-10-23 17:47:29 +0300

    add

--- init.lua
+++ init.lua
@@ -31,7 +31,7 @@ end
 
 -- screen split
 
-vim.keymap.set("n", "<C-v>", function()
+vim.keymap.set("n", "<C-b>", function()
   local wins = vim.api.nvim_list_wins()
   if #wins > 1 then
     vim.cmd("close")
@@ -56,3 +56,29 @@ vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<CR>", { desc = "Find fil
 vim.keymap.set("n", "<C-d>", "yyp", { desc = "Duplicate current line" })
 
 -- duplicate current line
+--
+--
+--
+--
+--
+--
+--
+--
+--
+
+
+
+
+
+
+-- copy and paste -- 
+
+-- Copy with Ctrl + C in visual mode
+vim.keymap.set("v", "<C-c>", '"+y', { desc = "Copy to system clipboard" })
+
+-- Paste with Ctrl + V in normal mode
+vim.keymap.set("n", "<C-v>", '"+p', { desc = "Paste from system clipboard" })
+
+-- Paste with Ctrl + V in insert mode
+vim.keymap.set("i", "<C-v>", '<Esc>"+pa', { desc = "Paste from system clipboard" })
+
